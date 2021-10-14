fbe01e8ecc3e0954165bf4a895d689e0f9778bd7
Yukai Huang <yukaihuangtw@gmail.com>
2021-10-14 11:26:17 +0800

    Render Client side mathjax

--- components/Markdown.js
+++ components/Markdown.js
@@ -1,21 +1,80 @@
 import { render } from 'lib/markdown'
+import Script from 'next/script'
 import useCopySnippet from 'lib/hooks/useCopySnippet'
+import { useEffect, useRef, useState } from 'react'
 
 const Markdown = ({ content, className = '', ...props }) => {
   useCopySnippet()
 
+  /**
+   * @type {import('react').MutableRefObject<HTMLDivElement | null>}
+   */
+  const articleRef = useRef()
+  const [mathjaxdivs, setMathjaxdivs] = useState([])
+  useEffect(() => {
+    if (!articleRef.current) {
+      setMathjaxdivs([])
+    } else {
+      setMathjaxdivs(
+        Array.from(articleRef.current.querySelectorAll('span.mathjax.raw'))
+      )
+    }
+  }, [setMathjaxdivs, articleRef])
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && !window.MathJax) {
+      return
+    }
+
+    const MathJax = window.MathJax
+
+    MathJax.Hub.Config({
+      jax: ['input/TeX', 'output/CommonHTML'],
+      extensions: [
+        'tex2jax.js',
+        'MathMenu.js',
+        'MathZoom.js',
+        'AssistiveMML.js',
+        'a11y/accessibility-menu.js',
+        'Safe.js',
+      ],
+      TeX: {
+        extensions: [
+          'AMSmath.js',
+          'AMSsymbols.js',
+          'noErrors.js',
+          'noUndefined.js',
+        ],
+      },
+    })
+
+    if (mathjaxdivs.length) {
+      MathJax.Hub.Queue(['Typeset', MathJax.Hub, mathjaxdivs])
+      MathJax.Hub.Queue(() => {
+        mathjaxdivs.forEach((div) => div.classList.remove('raw'))
+      })
+    }
+  }, [mathjaxdivs])
+
   return (
-    <main id="main" property="schema:mainEntity">
-      <article
-        itemScope
-        itemProp="post"
-        typeof="schema:Article schema:BlogPosting"
-        property="schema:articleBody"
-        className={`markdown-body ${className}`}
-        dangerouslySetInnerHTML={{ __html: render(content) }}
-        {...props}
+    <>
+      <main id="main" property="schema:mainEntity" ref={articleRef}>
+        <article
+          itemScope
+          itemProp="post"
+          typeof="schema:Article schema:BlogPosting"
+          property="schema:articleBody"
+          className={`markdown-body ${className}`}
+          dangerouslySetInnerHTML={{ __html: render(content) }}
+          {...props}
+        />
+      </main>
+
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-AMS_CHTML"
+        strategy="lazyOnload"
       />
-    </main>
+    </>
   )
 }
 
